a5726c7e29a815f520e551ac8cd8e46ee793b2e7
Yermi <kycasdzxc@naver.com>
2023-06-03 23:56:15 +0900

    디지털시계, 검색바, 명언

---
+++ js/quote.js
@@ -0,0 +1,37 @@
+const API_URL =
+  "https://port-0-random-quote-4uvg2mleme84ru.sel3.cloudtype.app/";
+
+const quoteElement = document.getElementById("quote");
+const quoteItem = localStorage.getItem("quote");
+
+const nowDate = new Date();
+const month = nowDate.getMonth() + 1;
+const date = nowDate.getDate();
+
+const setQuote = (result) => {
+  let quote = { createDate: `${month}-${date}`, quote: result };
+  localStorage.setItem("quote", JSON.stringify(quote));
+  quoteElement.textContent = result;
+};
+
+const getQuote = async () => {
+  try {
+    const data = await fetch(API_URL).then((res) => res.json());
+    const result = data[1].respond;
+    setQuote(result);
+  } catch (err) {
+    console.log(`err:${err}`);
+    setQuote("작은 기회로 부터 종종 위대한 업적이 시작된다. - 데모스테네스");
+  }
+};
+
+if (quoteItem) {
+  let { createDate, quoteData } = JSON.parse(quoteItem);
+  if (createDate === `${month}-${date}`) {
+    quoteElement.textContent = `"${quoteData}"`;
+  } else {
+    getQuote();
+  }
+} else {
+  getQuote();
+}
---
+++ js/search.js
@@ -0,0 +1,17 @@
+const searchInput = document.getElementById("search-input");
+
+const showSearchResult = () => {
+  let searchWord = searchInput.value;
+  location.href = `https://www.google.com/search?q=${searchWord}`;
+  searchWord = "";
+};
+
+const enterKey = (event) => {
+  if (event.code === "Enter") {
+    showSearchResult();
+  }
+};
+
+searchInput.addEventListener("keypress", (event) => {
+  enterKey(event);
+});
